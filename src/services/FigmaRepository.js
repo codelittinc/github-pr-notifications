b0b129d569084c325a4627955b5778c5a5059aed
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2020-02-13 15:19:58 -0300

    Update figma repository to have the info of all projects

--- src/services/FigmaRepository.js
+++ src/services/FigmaRepository.js
@@ -1,7 +1,43 @@
 const repositoriesIdentifiers = {
-  'zonda': {
+  'Team-Avison-Young': {
+    id: '4362930',
+    channel: 'feed-ay',
+  },
+  'Team-Blackbird': {
+    id: '2136589',
+    channel: 'team-blackbird',
+  },
+  'Team-Rolli': {
+    id: '3673059',
+    channel: 'team-rolli',
+  },
+  'Team-Sales-Marketing': {
+    id: '2087651',
+    channel: 'dept-marketing',
+  },
+  'Codelitt-Design': {
+    id: '2072212',
+    channel: 'dept-design',
+  },
+  'Team-Maker': {
+    id: '3123941',
+    channel: 'team-teammaker',
+  },
+  'Team-Codelitt-Website': {
+    id: '3468738',
+    channel: 'team-website',
+  },
+  'Team-Web-CMS': {
+    id: '1872015',
+    channel: 'team-acst-websites',
+  },
+  'Team-Video-Streaming': {
+    id: '2539181',
+    channel: 'team-video-stream',
+  },
+  'Team-Zonda': {
     id: '3825282',
-    channel: 'test-channel',
+    channel: 'team-zonda',
   },
 }
 
