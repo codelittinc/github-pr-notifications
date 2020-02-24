4e29b249cbc169c388b2e614f0aa62c7dd2a306e
kaiomagalhaes <me@kaiomagalhaes.com>
2020-02-24 19:19:58 -0300

    improve

--- src/Todoist.js
+++ src/Todoist.js
@@ -17,13 +17,16 @@ const reviewProjectDaily = {
 const taskTemplates = [
   {
     projectName: "Codelitt Recurring",
-    due: 'today at 9am', 
+    due: 'today at 9am',
     task: {
       name: "Start day"
     },
     subtasks: [
       {
         name: "Start Toggle",
+      },
+      {
+        name: "Review calendar and prepare tasks for any meeting",
       },
       {
         name: "Review work mentions",
@@ -38,7 +41,7 @@ const taskTemplates = [
   },
   {
     projectName: "Codelitt Recurring",
-    due: 'today at 5pm', 
+    due: 'today at 5pm',
     task: {
       name: "End day"
     },
@@ -53,9 +56,9 @@ const taskTemplates = [
   },
   {
     projectName: "Codelitt Recurring",
-    due: 'today at 9am', 
+    due: 'today at 9am',
     task: {
-      name: `Review Project ${reviewProjectDaily[new Date().toLocaleString('en-us', {  weekday: 'short' })]}`
+      name: `Review Project ${reviewProjectDaily[new Date().toLocaleString('en-us', { weekday: 'short' })]}`
     },
     subtasks: [
       {
@@ -87,9 +90,29 @@ class Todoist {
   }
 
   static async createTask(name, projectId, due, parentId) {
+    const dueObject = {
+      string: due
+    };
+
+    const commands = [
+      {
+        type: 'item_add',
+        temp_id: uuid.v4(),
+        uuid: uuid.v4(),
+        args: {
+          content: name,
+          project_id: projectId,
+          parent_id: parentId,
+          due: (due ? dueObject : {})
+        }
+      }
+    ]
+    const stringCommands = JSON.stringify(commands)
     const url = getUrl(
-      `&commands=[{"type": "item_add", "temp_id": "${uuid.v4()}", "uuid": "${uuid.v4()}", "args": {"content": "${name}", "project_id": ${projectId}, "parent_id": "${parentId}", "due": {"string": "${due}"}}}]`
-      );
+      `&commands=${stringCommands}`
+    );
+    console.log(url)
+
     const response = await axios.get(url);
     return response.data;
 
@@ -104,7 +127,7 @@ class Todoist {
       const parentId = Object.keys(todoistTask.temp_id_mapping)[0]
 
       subtasks.forEach((subtask) => {
-        this.createTask(subtask.name, project.id, due, parentId);
+        this.createTask(subtask.name, project.id, undefined, parentId);
       })
     }
   }
