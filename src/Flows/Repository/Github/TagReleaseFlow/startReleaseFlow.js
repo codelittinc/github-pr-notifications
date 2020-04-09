c899952d1a17312cd5f065ff98e2c35cb1e62089
kaiomagalhaes <me@kaiomagalhaes.com>
2020-04-09 18:13:36 -0300

    fix release changelog

--- src/Flows/Repository/Github/TagReleaseFlow/startReleaseFlow.js
+++ src/Flows/Repository/Github/TagReleaseFlow/startReleaseFlow.js
@@ -3,25 +3,31 @@ import { Github, GithubCommits, Slack } from '../../../../services/index.js'
 export default async (deployChannel, releases, latestRelease, owner, repository) => {
   let oldestPrerelease;
   let index = 0;
+  let oldestRelease;
 
   while (!oldestPrerelease) {
     if (!releases[index + 1] || !releases[index + 1].prerelease) {
       oldestPrerelease = releases[index]
+
+      if (releases[index + 1]) {
+        oldestRelease = releases[index + 1];
+      }
     } else {
       index++
     }
   }
-
   const { tag_name } = latestRelease;
+
+  let text;
   const oldestPreleaseCommits = (await Github.listBranchCommits({
     owner,
     repo: repository,
-    branch: oldestPrerelease.tag_name
+    branch: (oldestRelease || oldestPrerelease).tag_name
   }))
 
-  const text = await GithubCommits.getCommitMessagesText({
+  text = await GithubCommits.getCommitMessagesText({
     head: latestRelease.tag_name,
-    base: oldestPreleaseCommits[1].sha,
+    base: oldestPreleaseCommits[0].sha,
     owner,
     repository,
   });
@@ -44,9 +50,10 @@ export default async (deployChannel, releases, latestRelease, owner, repository)
       body: `Available in this release \n ${text}`,
       prerelease: false
     });
-    Slack.getInstance().sendMessage({
-      message: `Available in this release \n${text}`,
-      channel: text
+
+    await Slack.getInstance().sendMessage({
+      message: `*Available in this release* \n${text}`,
+      channel: deployChannel
     });
   } else {
 
