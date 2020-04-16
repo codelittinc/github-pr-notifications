21702441dcfe16ec7553e3b19b3683b605bdcc4c
kaiomagalhaes <me@kaiomagalhaes.com>
2020-04-16 08:58:38 -0300

    house keeping

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import dotenv from 'dotenv'
-import axios from 'axios';
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 import bodyParser from 'body-parser';
 import express from 'express';
 import {
-  HomeController,
   DeployNotificationController,
+  FlowsController,
+  HomeController,
   PullRequestsController,
   SlackController,
-  FlowsController
 } from './controllers';
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.get('/', HomeController.index)
-app.get(`/open-prs/:devGroup?`, PullRequestsController.index)
+app.get('/', HomeController.index);
+app.get(`/open-prs/:devGroup?`, PullRequestsController.index);
 
-app.post('/', FlowsController.create)
-app.post('/flows', FlowsController.create)
+app.post('/', FlowsController.create);
+app.post('/flows', FlowsController.create);
 app.post('/notify-deploy', DeployNotificationController.create);
-app.post('/slack', SlackController.create)
+app.post('/slack', SlackController.create);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
