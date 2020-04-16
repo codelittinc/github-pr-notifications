392f318f416bea7ac7c46f27a9ac4a7cb51b54f6
kaiomagalhaes <me@kaiomagalhaes.com>
2020-04-16 08:52:53 -0300

    remove mentions code

--- src/index.js
+++ src/index.js
@@ -26,19 +26,4 @@ app.post('/flows', FlowsController.create)
 app.post('/notify-deploy', DeployNotificationController.create);
 app.post('/slack', SlackController.create)
 
-app.post('/mentions', async (req, res) => {
-  const { event } = req.body;
-  if (event) {
-    const { channel, text } = event;
-
-    const url = 'https://hooks.zapier.com/hooks/catch/4254966/o547s18';
-
-    await axios.post(url, {
-      text,
-    });
-  }
-
-  res.sendStatus(200)
-})
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
