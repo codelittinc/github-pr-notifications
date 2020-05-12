6233fb4ed08ac66928b9f69bb7160d64151f91f8
kaiomagalhaes <me@kaiomagalhaes.com>
2020-05-12 18:13:45 -0300

    add error catching to deploy notifications

--- src/controllers/DeployNotificationController.js
+++ src/controllers/DeployNotificationController.js
@@ -1,4 +1,5 @@
 import ServerFlow from '../Flows/Server/ServerFlow';
+import { SlackRepository, Slack } from '@services'
 
 export default class DeployNotificationController {
   static async create(req, res) {
@@ -8,7 +9,14 @@ export default class DeployNotificationController {
     const flowName = flow.constructor.name;
 
     console.log(`Start: ${flowName}`)
-    await flow.run()
+    try {
+      await flow.run()
+    } catch (e) {
+      Slack.getInstance().sendDirectMessage({
+        message: JSON.stringify(this.data),
+        username: SlackRepository.getAdminSlackUser()
+      });
+    }
     console.log(`End: ${flowName}`)
 
     res.sendStatus(200);
